5a6f5c670174e241d599dc1664a0005945c5a654
notmaggiem-del <notmaggiem@gmail.com>
2025-11-09 10:21:12 -0500

    referencing motors.java in UsingMotors.java - it should move the motor at 0.5 power when init is pressed on the driver hub.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UsingMotors.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class UsingMotors extends OpMode {
+
+    //si quieres conectar a robot, toca el buton desde "TeamCode"
+    //luego lo construye (build)
+
+    //make sure control hub is connected to computer either
+    //via usb or wifi
+
+    motors bench = new motors();
+
+    @Override
+    public void init(){
+        bench.init(hardwareMap);
+        //init config
+    }
+    @Override
+    public void loop(){
+        bench.setMotorSpeed(0.5);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class motors {
+
+    private DcMotor test_motor;
+    //depends on config
+
+    public void init(HardwareMap hwMap) {
+        //insert touch sens
+
+        test_motor = hwMap.get(DcMotor.class, "test_motor");
+        test_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setMotorSpeed(double speed) {
+        test_motor.setPower(speed);
+    }
+}
