3932f6277b2937f02ae6dca4b436625e11f57538
notmaggiem-del <notmaggiem@gmail.com>
2026-01-06 10:13:59 -0500

    Pasted pedropathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -1,21 +1,4 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Test {
-
-    private DcMotor test_motor;
-
-    public void init(hardwareMap hwMap) {
-
-        //reference configs (test_motor
-        test_motor = hwMap.get(DcMotor.class, "test_motor");
-        test_motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        //run using -- both will run at same speed (higher prob
-        // run using --counter
-
-    }
-
-    public void setMotorSpeed(double speed) {
-        //value -1-1
-        test_motor.setPower(speed);
-    }
 }
