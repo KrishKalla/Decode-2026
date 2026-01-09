b3b9a28f2e379d6b59556742db3aac25927f913e
HBaconburger <hbreed120@icloud.com>
2026-01-09 18:56:49 -0500

    huh?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huh.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huh.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
-public class huh {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Autonomous
+public class huh extends LinearOpMode {
+    DcMotor funky=null;
+    DcMotor topright=null;
+    DcMotor bottomleft=null;
+    DcMotor bottomright=null;
+    public void runOpMode(){
+        funky=hardwareMap.get(DcMotor.class,"funky");
+        telemetry.addData("Does this work?","please work");
+        funky.setPower(4);
+
+    }
 }
