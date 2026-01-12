2fd455b4ca2ee0e9ce5575a7fcff74e17310e5e9
Krish Kalla <krish.k.2626@gmail.com>
2026-01-11 20:14:10 -0500

    Completed Intake Files + Re-Gradle Distribution * Extension Servo Positions require tuning * Motor Power set 0.8 via constants.java * Reversal T/F unknown (set to false currently)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.util.constants;
+
+public class intake {
+
+    private DcMotorEx motorL;
+    private DcMotorEx motorR;
+    public Servo servoL;
+    public Servo servoR;
+
+    private DcMotorEx.Direction defaultL;
+    private DcMotorEx.Direction defaultR;
+
+    private String intakeState;
+    private boolean extended;
+
+    public intake(){
+        intakeState = "INSTANTIATED";
+        extended = false;
+    }
+
+    public void init(HardwareMap map) {
+        intakeState = "INIT";
+
+        motorL = map.get(DcMotorEx.class, "intakeL");
+        motorL.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        motorR = map.get(DcMotorEx.class, "intakeR");
+        motorR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        servoL = map.get(Servo.class, "extensionL");
+        servoR = map.get(Servo.class, "extensionR");
+
+        if (constants.REVERSED) {
+            motorL.setDirection(DcMotorEx.Direction.REVERSE);
+            servoL.setDirection(Servo.Direction.REVERSE);
+        } else {
+            motorR.setDirection(DcMotorEx.Direction.REVERSE);
+            servoR.setDirection(Servo.Direction.REVERSE);
+        }
+
+        defaultL = motorL.getDirection();
+        defaultR = motorR.getDirection();
+    }
+
+    public void setIntake(constants.INTAKE state) {
+        switch(state) {
+            case ON:
+                intakeState = "ON";
+                setDirection(1);
+                setPower(constants.INTAKE_POWER);
+                setExtension(constants.INTAKE_EXTENSION.EXTENDED);
+                break;
+            case OFF:
+                intakeState = "OFF";
+                setDirection(1);
+                setPower(0);
+                setExtension(constants.INTAKE_EXTENSION.RETRACTED);
+                break;
+            case REJECT:
+                intakeState = "REJECT";
+                setDirection(-1);
+                setPower(constants.INTAKE_POWER);
+                setExtension(constants.INTAKE_EXTENSION.RETRACTED);
+                break;
+        }
+    }
+
+    public void setExtension(constants.INTAKE_EXTENSION state) {
+        switch(state) {
+            case RETRACTED:
+                extended = false;
+                servoL.setPosition(state.left);
+                servoR.setPosition(state.right);
+                break;
+            case EXTENDED:
+                extended = true;
+                servoL.setPosition(state.left);
+                servoR.setPosition(state.right);
+                break;
+        }
+    }
+
+
+    private void setDirection(int direction) {
+        if (direction == -1) {
+            motorL.setDirection(flip(motorL.getDirection()));
+            motorR.setDirection(flip(motorR.getDirection()));
+        }
+
+        else {
+            motorL.setDirection(defaultL);
+            motorR.setDirection(defaultR);
+        }
+    }
+
+    private void setPower(double d) {
+        motorL.setPower(d);
+        motorR.setPower(d);
+    }
+
+    private DcMotorEx.Direction flip(DcMotorEx.Direction dir) {
+        return (dir == DcMotorEx.Direction.FORWARD)
+                ? DcMotorEx.Direction.REVERSE
+                : DcMotorEx.Direction.FORWARD;
+    }
+
+    public String getIntakeState() {
+        return intakeState;
+    }
+
+    public boolean isExtended() {
+        return extended;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/constants.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class constants {
+
+    //Intake Constants
+    public static boolean REVERSED = false;
+    public static double INTAKE_POWER = 0.8;
+    public enum INTAKE {
+        ON,
+        OFF,
+        REJECT
+    }
+
+    public enum INTAKE_EXTENSION {
+        RETRACTED(0, 1),
+        EXTENDED (1, 0);
+
+        public final double left;
+        public final double right;
+
+        INTAKE_EXTENSION(double l, double r) {
+            this.left = l;
+            this.right = r;
+        }
+    }
+
+
+
+}
