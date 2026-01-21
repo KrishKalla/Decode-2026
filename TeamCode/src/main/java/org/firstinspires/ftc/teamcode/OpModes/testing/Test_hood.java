78711841732c43749530cab9087985466003b74c
Jack-Xie-Akioi <xjz22789@gmail.com>
2026-01-21 13:09:36 -0500

    krish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/testing/Test_hood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/testing/Test_hood.java
@@ -7,20 +7,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Left Servo Hood", group = "Op_Tests")
 @Config
-public class Test_left_servo extends LinearOpMode {
+public class Test_hood extends LinearOpMode {
 
     public static double position = 0.5;
 
     @Override
     public void runOpMode() {
 
+        Servo rightHood = hardwareMap.get(Servo.class, "rightHood");
         Servo leftHood = hardwareMap.get(Servo.class, "leftHood");
 
         waitForStart();
 
         while (opModeIsActive()) {
             leftHood.setPosition(position);
-
+            rightHood.setPosition(position);
         }
     }
 }
