e920d3ba73a865850422cc47be0cb17afa2c91ad
Aaditya <abilak@gmail.com>
2026-01-22 10:43:21 -0500

    krish admits 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/constants.java
@@ -65,11 +65,11 @@ public class constants {
     //Shooter Constants
     @Config
     public static class shooter {
-       public static double kP = 0.00115;
+       public static double kP = 0.0012;
        public static double kI = 0;
-       public static double kD = 0.00011;
-       public static double kF = 0.00045;
-       public static double TARGET_RPM = 1800;
+       public static double kD = 0.00003;
+       public static double kF = 0.000412;
+       public static double TARGET_RPM = 1650;
        public static double alpha = 0.225;
        public static double step = 0.05;
        public static double MIN_ANGLE = 0.165;
