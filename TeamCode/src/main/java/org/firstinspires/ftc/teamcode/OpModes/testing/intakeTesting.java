ef837510735b8583d711c76a2b8f4a200770411e
Jack-Xie-Akioi <xjz22789@gmail.com>
2026-01-23 13:11:48 -0500

    krish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/testing/intakeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/testing/intakeTesting.java
@@ -5,17 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "old shooter test", group = "Op_Tests")
+@TeleOp(name = "Intake test", group = "Op_Tests")
 @Config
-public class Test_shooter extends LinearOpMode {
+public class intakeTesting extends LinearOpMode {
 
     public static double power = 0.0;
 
     @Override
     public void runOpMode() {
 
-        DcMotorEx leftShooter = hardwareMap.get(DcMotorEx.class, "leftShooter");
-        DcMotorEx rightShooter = hardwareMap.get(DcMotorEx.class, "rightShooter");
+        DcMotorEx leftShooter = hardwareMap.get(DcMotorEx.class, "leftIntake");
+        DcMotorEx rightShooter = hardwareMap.get(DcMotorEx.class, "rightIntake");
         leftShooter.setDirection(DcMotorEx.Direction.REVERSE);
         leftShooter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         rightShooter.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
