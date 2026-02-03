987d8ae134e2a8a565815a5afcb99ebdd841a47b
Jack-Xie-Akioi <xjz22789@gmail.com>
2026-02-03 17:01:46 -0500

    update constant and auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/auto.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.OpModes.Auto;
 
-import static android.os.SystemClock.sleep;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.follower.Follower;
@@ -10,6 +8,7 @@ import com.pedropathing.geometry.Pose;
 import com.pedropathing.geometry.BezierLine;
 
 
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.subsystems.intake;
 import org.firstinspires.ftc.teamcode.subsystems.shooter;
 import org.firstinspires.ftc.teamcode.subsystems.turret;
@@ -22,7 +21,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @Autonomous(name = "Test Close Auto")
-public class auto extends OpMode {
+public class Auto extends OpMode {
 
     private LLHandler llHandler;
 
@@ -31,21 +30,20 @@ public class auto extends OpMode {
 
     // ---- State System ----
     private int pathState = 0;
-    
-    private final Pose startPose    = new Pose(20.5, 118, Math.toRadians(180));
-    private final Pose scorePose    = new Pose(55.000,  84.000, Math.toRadians(180));
-    private final Pose pickup1Pose  = new Pose(23.000,  84.000, Math.toRadians(180));
-    private final Pose pickup2Pose  = new Pose(23.000,  60.000, Math.toRadians(180));
-    private final Pose pickup3Pose  = new Pose(23.000,  36.000, Math.toRadians(180));
-    private final Pose midPickup2   = new Pose(79.000,  57.000, Math.toRadians(180));
-    private final Pose midPickup3   = new Pose(74.135, 29.993, Math.toRadians(180));
+
+    private final Pose startPose = new Pose(26.1, 128.6, Math.toRadians(145));
+    private final Pose scorePose = new Pose(55.000, 84.000, Math.toRadians(180));
+    private final Pose pickup1Pose = new Pose(23.000, 84.000, Math.toRadians(180));
+    private final Pose pickup2Pose = new Pose(23.000, 60.000, Math.toRadians(180));
+    private final Pose pickup3Pose = new Pose(23.000, 36.000, Math.toRadians(180));
+    private final Pose midPickup2 = new Pose(79.000, 57.000, Math.toRadians(180));
+    private final Pose midPickup3 = new Pose(74.000, 30.000, Math.toRadians(180));
 
     // ---- PATH OBJECTS ----
     private Path scorePreload;
     private PathChain grabPickup1, scorePickup1;
     private PathChain grabPickup2, scorePickup2;
     private PathChain grabPickup3, scorePickup3;
-
 
 
     @Override
@@ -56,39 +54,44 @@ public class auto extends OpMode {
         shooter shooter = new shooter();
         intake intake = new intake();
         turret turret = new turret();
-        constants.shooter.TARGET_RPM=800;
-        constants.shooter.Hood_pos=0.75;
+        follower = Constants.createFollower(hardwareMap);
+        follower.setPose(startPose);
 
-        shooter.init(hardwareMap,llHandler);
+        constants.shooter.TARGET_RPM = 800;
+        constants.shooter.Hood_pos = 0.75;
+
+        shooter.init(hardwareMap, llHandler);
         intake.init(hardwareMap);
-        turret.init(hardwareMap,llHandler);
+        turret.init(hardwareMap, llHandler);
 
+        addTelemetry("position", follower.getPose());
+        
         buildPaths();
     }
 
     @Override
     public void loop() {
         follower.update();
+        autonomousPathUpdate();
+        telemetry.update();
     }
 
 
     // ---- BUILD PATHS ----
     private void buildPaths() {
 
-
         scorePreload = new Path(new BezierLine(startPose, scorePose));
         scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
 
         grabPickup1 = follower.pathBuilder()
                 .addPath(new BezierLine(scorePose, pickup1Pose))
-                .setTangentHeadingInterpolation()  // smooth into pickup
+                .setConstantHeadingInterpolation(Math.toRadians(180))
                 .build();
 
 
         scorePickup1 = follower.pathBuilder()
                 .addPath(new BezierLine(pickup1Pose, scorePose))
-                .setTangentHeadingInterpolation()
-                .setReversed()
+                .setConstantHeadingInterpolation(Math.toRadians(180))
                 .build();
 
 
@@ -99,7 +102,6 @@ public class auto extends OpMode {
                         pickup2Pose
                 ))
                 .setConstantHeadingInterpolation(Math.toRadians(180))
-                .setReversed()
                 .build();
 
 
@@ -133,53 +135,55 @@ public class auto extends OpMode {
                 setPathState(1);
                 break;
             case 1:
-                if(!follower.isBusy()) {
+                if (!follower.isBusy()) {
                     // Score Preload
-                    follower.followPath(grabPickup1,true);
+                    follower.followPath(grabPickup1, true);
                     setPathState(2);
                 }
                 break;
             case 2:
-                if(!follower.isBusy()) {
-                    follower.followPath(scorePickup1,true);
+                if (!follower.isBusy()) {
+                    follower.followPath(scorePickup1, true);
                     setPathState(3);
                 }
                 break;
             case 3:
-                if(!follower.isBusy()) {
-                    follower.followPath(grabPickup2,true);
+                if (!follower.isBusy()) {
+                    follower.followPath(grabPickup2, true);
                     setPathState(4);
                 }
                 break;
             case 4:
-                if(!follower.isBusy()) {
-                    follower.followPath(scorePickup2,true);
+                if (!follower.isBusy()) {
+                    follower.followPath(scorePickup2, true);
                     setPathState(5);
                 }
                 break;
             case 5:
-                if(!follower.isBusy()) {
-                    follower.followPath(grabPickup3,true);
+                if (!follower.isBusy()) {
+                    follower.followPath(grabPickup3, true);
                     setPathState(6);
                 }
                 break;
             case 6:
-                if(!follower.isBusy()) {
-                    follower.followPath(scorePickup3,true);
+                if (!follower.isBusy()) {
+                    follower.followPath(scorePickup3, true);
                     setPathState(7);
                 }
                 break;
             case 7:
-                if(!follower.isBusy()) {
+                if (!follower.isBusy()) {
                     setPathState(-1); // Done
                 }
                 break;
         }
     }
 
-    public void setPathState(int pState) {
-        pathState = pState;
-    }
+    public void setPathState(int pState) { pathState = pState; }
 
+
+    public void addTelemetry(String info, Object value) {
+        telemetry.addData(info, value);
+    }
 
 }
