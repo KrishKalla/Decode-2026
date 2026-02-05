7b9be35b448fef4b196ffdbbd4850692609504b7
Jack-Xie-Akioi <xjz22789@gmail.com>
2026-02-05 09:21:40 -0500

    kim zhonh wu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/v1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opModes.TeleOp;
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
